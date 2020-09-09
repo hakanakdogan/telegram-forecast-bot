d55373f1be8f19d1ca44687640de39b870804acf
Hakan Akdoğan <akdoganhakann@gmail.com>
2020-09-09 21:33:46 +0300

    Turkish language support update

--- src/bot.js
+++ src/bot.js
@@ -31,13 +31,13 @@ bot.onText(/\/havadurumu (.+)/ , (msg,match)=>{
                         error
                     });
                 }else{
-                    translate(response.weather,(error,response)=>{
+                    translate(response.weather,(error,data)=>{
                         if(error){
                             return console.log(error);
                         }else{
-                            parseString(response, (err,result)=>{
+                            parseString(data, (err,result)=>{
                                 
-                                bot.sendMessage(chatId, `TR: Mekan: ${text}\nEN: Location: ${text}\n\n${result.string._}`);
+                                bot.sendMessage(chatId, `TR: Mekan: ${text}\nEN: Location: ${text}\n\nTR: Hava gün boyunca ${result.string._} olacak.\nŞu an sıcaklık ${response.temperature} derece.\nHissedilen sıcaklık ise ${response.feelslike}derece.\nNem yüzdesi ise %${response.humidity}\n\nEN: It is ${response.weather} throughout the day.\nIt is currently ${response.temperature} degrees out.\nIt feels like ${response.feelslike} degrees out.\nHumidity is %${response.humidity} right now.`);
                             })
                             
                         }
