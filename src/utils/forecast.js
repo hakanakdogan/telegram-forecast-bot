bb1b5a0061c61df284182d39c33b96ceb3abd8b8
Hakan Akdoğan <akdoganhakann@gmail.com>
2020-09-09 19:55:30 +0300

    Turkce dil destegi

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,22 +6,26 @@ const forecast = (lat , long ,callback)=>{
 
     request({url , json:true} , (error , {body})=>{
         const currentWeather = body.current;
-        let ceviri="";
-        translate(currentWeather.weather_descriptions[0], (error, response) => {
-            if (error) {
-                return console.log(error);
-            }
-
-            ceviri = response[0].translations[0].text;
-
-        });
+        
+        
 
         if(error){
             callback("Unable to connect to forecast server" , undefined);
         }else if(body.error){
             callback({errType:body.error.type,errInfo:response.body.error.info} , undefined)
         }else{
-            callback(undefined ,  `TR: Hava gün boyunca ${ceviri} olacak.\nHava şu an ${currentWeather.temperature} derece.\nHissedilen sıcaklık ${currentWeather.feelslike}\nNem ise şu an %${currentWeather.humidity} \n\nEN: It is ${currentWeather.weather_descriptions[0]} throughout the day.\nIt is currently ${currentWeather.temperature} degrees out.\nIt feels like ${currentWeather.feelslike} degrees out.\nHumidity is %${currentWeather.humidity} right now.`);
+            translate(currentWeather.weather_descriptions[0], (error, response) => {
+                if (error) {
+                    return console.log(error);
+                }
+    
+                const ceviri = response[0].translations[0].text;
+                callback(undefined ,  `TR: Hava gün boyunca ${ceviri} olacak.\nHava şu an ${currentWeather.temperature} derece.\nHissedilen sıcaklık ${currentWeather.feelslike}\nNem ise şu an %${currentWeather.humidity} \n\nEN: It is ${currentWeather.weather_descriptions[0]} throughout the day.\nIt is currently ${currentWeather.temperature} degrees out.\nIt feels like ${currentWeather.feelslike} degrees out.\nHumidity is %${currentWeather.humidity} right now.`);
+            });
+
+
+
+            
         }
 
     });
