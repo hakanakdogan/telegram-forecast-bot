371e99bd578e8078eeadfc0fa7f1894393c2b7f9
Hakan Akdoğan <akdoganhakann@gmail.com>
2020-09-09 20:41:21 +0300

    .

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,13 @@
 const request = require("request");
 const translate = require("./translate");
+const parseString = require("xml2js").parseString;
 
 const forecast = (lat , long ,callback)=>{
     const url = `http://api.weatherstack.com/current?access_key=cd88b468d1eeeffd9a5367134c724ac1&query=${encodeURI(lat)},${encodeURI(long)}&units=m`;
 
     request({url , json:true} , (error , {body})=>{
         const currentWeather = body.current;
-        
+        const ceviri="";
         
 
         if(error){
@@ -14,13 +15,18 @@ const forecast = (lat , long ,callback)=>{
         }else if(body.error){
             callback({errType:body.error.type,errInfo:response.body.error.info} , undefined)
         }else{
-            translate(currentWeather.weather_descriptions[0], (error, response) => {
-                if (error) {
+            
+            translate(currentWeather.weather_descriptions[0],(error,response)=>{
+                if(error){
                     return console.log(error);
                 }
-    
-                const ceviri = response[0].translations[0].text;
+                parseString(response, (err,result)=>{
+                    ceviri =result.string._;
+                });
+
                 callback(undefined ,  `TR: Hava gün boyunca ${ceviri} olacak.\nHava şu an ${currentWeather.temperature} derece.\nHissedilen sıcaklık ${currentWeather.feelslike}\nNem ise şu an %${currentWeather.humidity} \n\nEN: It is ${currentWeather.weather_descriptions[0]} throughout the day.\nIt is currently ${currentWeather.temperature} degrees out.\nIt feels like ${currentWeather.feelslike} degrees out.\nHumidity is %${currentWeather.humidity} right now.`);
+                
+            
             });
 
 
--- src/utils/translate.js
+++ src/utils/translate.js
@@ -2,28 +2,27 @@ const request = require("request");
 const translate =(forecast,callback)=>{
     
 
-    const options = {
-    method: 'POST',
-    url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
-    qs: {profanityAction: 'NoAction', textType: 'plain', to: 'tr', 'api-version': '3.0'},
-    headers: {
-        'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
-        'x-rapidapi-key': '7e4c8a728emshe997bc7b04c32ffp10de0ejsn5ea99e0373c7',
-        'content-type': 'application/json',
-         accept: 'application/json',
-        useQueryString: true
-    },
-    body: [{Text: forecast}],
-    json: true
-};
+    var options = {
+        method: 'GET',
+        url: 'https://microsoft-azure-translation-v1.p.rapidapi.com/translate',
+        qs: {from: 'en', to: 'tr', text: forecast},
+        headers: {
+          'x-rapidapi-host': 'microsoft-azure-translation-v1.p.rapidapi.com',
+          'x-rapidapi-key': '7e4c8a728emshe997bc7b04c32ffp10de0ejsn5ea99e0373c7',
+          accept: 'application/json',
+          useQueryString: true
+        }
+      };
+      
+      request(options, function (error, response, body) {
+        if (error){
+            callback(error,undefined);
+        }
+    
+        callback(undefined , body);
+      });
 
-request(options, (error, response, body)=> {
-	if (error){
-        callback(error,undefined);
-    }
 
-	callback(undefined , body);
-});
 }
 
 
